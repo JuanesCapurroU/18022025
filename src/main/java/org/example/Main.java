d88ac32c278cf6c660c7a8b6dd539dc320b58c74
JuanesCapurroU <juan.capurro@uniminuto.edu.co>
2025-02-19 16:53:53 -0500

    Informacion Parental

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Juan Esteban Capurro Buitrago");
-
         System.out.println("Tengo un gato llamado Zeus");
+        System.out.println("Mi mama se llama MARTHA");
+        System.out.println("Mi padre se llama Leon");
+        System.out.println("Hola Mundo!");
 
     }
 }
