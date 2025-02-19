1824119d8969924460cd3860e622cac7b8d6e8fa
JuanesCapurroU <juan.capurro@uniminuto.edu.co>
2025-02-19 11:25:47 -0500

    Descripción de Juanes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Juan Esteban Capurro Buitrago");
+        System.out.println("Tengo 22 años");
+        System.out.println("Vivo en Cajica, pero naci en Bogota");
+        System.out.println("Me gusta Jugar Futbol, videojuegos y compartir con mis conocidos.");
 
     }
 }
